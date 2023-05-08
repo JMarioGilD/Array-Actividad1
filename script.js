3d94992f173596a18bc1c6e7342d508e3205cca9
JMarioGilD <jmariogild88@gmail.com>
2023-05-08 11:33:34 -0500

    Actividad Array mis bandas favoritas

---
+++ script.js
@@ -0,0 +1,31 @@
+// Comencemos con una práctica básica de arrays .
+
+// En esta tarea, nos gustaría que crearas un array de tres elementos, almacenados dentro de una variable llamada myArray. 
+// Los artículos pueden ser lo que quieras, 
+// ¿qué tal tus comidas o bandas favoritas?
+
+let myArray = ["Mana", "Queen", "Epica"];
+
+// A continuación, modifica los dos primeros elementos del array utilizando una notación 
+// y asignación de corchetes simples [].
+
+ for(let n=0; n<myArray.length; n++){
+    myArray[0]='Coldplay';
+    myArray[1]='Muse';
+    console.log("array miArray", myArray[n]);
+}
+
+// Luego agrega un nuevo elemento al comienzo de la matriz.
+
+myArray.unshift("Maroon 5");
+console.log(myArray);
+
+// Luego crea una variable de tipo string donde puedas leer todos los elementos de tu array , separados por guiones -
+
+let BandasFaavoritas = 'Maroon5 Coldplay Muse Epica Mana Queen';
+let divide = BandasFaavoritas.split(' ');
+console.log(divide);
+
+for (let i=0; i<divide.length; i++){
+    document.write(divide[i] + " - ");
+}
